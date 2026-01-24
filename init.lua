8d548de2ad6b2ba1fa07a47b07f2412596074756
phil <philip.s.vernon@gmail.com>
2026-01-24 06:26:57 +0000

    test commit

--- init.lua
+++ init.lua
@@ -147,6 +147,7 @@ local is_godot_project = false
 local godot_project_path = ''
 local cwd = vim.fn.getcwd()
 
+
 -- iterate over paths and check
 for key, value in pairs(paths_to_check) do
 	if vim.uv.fs_stat(cwd .. value .. 'project.godot') then
