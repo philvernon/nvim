5361e11d5e940847f13683650a0142057e96ded7
philvernon <philip.s.vernon@gmail.com>
2026-06-01 17:41:35 +0100

    clean up keymaps

--- lua/phil/keymaps.lua
+++ lua/phil/keymaps.lua
@@ -1,41 +1,41 @@
-local opts = { noremap = true, silent = true }
+local opts = { silent = true }
 -- local keymap = vim.api.nvim_set_keymap
 local keymap = vim.keymap.set
 
 -- GENERAL
-keymap("n", "<leader>qq", ":wqa<CR>", opts)
+keymap("n", "<leader>qq", "<cmd>wqa<CR>", opts)
 
 -- Better window navigation
 keymap("n", "<Left>", "<C-w>h", opts)
 keymap("n", "<Down>", "<C-w>j", opts)
 keymap("n", "<Up>", "<C-w>k", opts)
 keymap("n", "<Right>", "<C-w>l", opts)
-keymap("n", "<leader>l", ":q<CR>", opts)
+keymap("n", "<leader>l", "<cmd>q<CR>", opts)
 -- open split
-keymap("n", "<leader>s", ":vsplit<CR>", opts)
-keymap("n", "<leader>S", ":split<CR>", opts)
+keymap("n", "<leader>s", "<cmd>vsplit<CR>", opts)
+keymap("n", "<leader>S", "<cmd>split<CR>", opts)
 -- buffer
-keymap("n", "<blash>q", ":bd<CR>", opts)
-keymap("n", "<leader><tab>", ":bn<CR>", opts)
-keymap("n", "<leader><s-tab>", ":bp<CR>", opts)
+keymap("n", "<blash>q", "<cmd>bd<CR>", opts)
+keymap("n", "<leader><tab>", "<cmd>bn<CR>", opts)
+keymap("n", "<leader><s-tab>", "<cmd>bp<CR>", opts)
 -- quickfix
-keymap("n", "]e", ":cnext<CR>", opts)
-keymap("n", "[e", ":cprev<CR>", opts)
+keymap("n", "]e", "<cmd>cnext<CR>", opts)
+keymap("n", "[e", "<cmd>cprev<CR>", opts)
 -- jk to exit insert mode
 -- keymap("i", "jk", "<ESC>", opts)
 -- keymap("i", "kj", "<ESC>", opts)
 -- keymap("i", "jj", "<ESC>", opts)
 -- keymap("i", "kk", "<ESC>", opts)
 -- Map alt + s to save
-keymap("n", "ß", "<ESC>:update<CR>", opts)
+keymap("n", "ß", "<cmd>update<CR>", opts)
 keymap("v", "ß", "<C-C>:update<CR><ESC>", opts)
 keymap("i", "ß", "<C-O>:update<CR><ESC>", opts)
 -- Map super + s to save on linux
-keymap("n", "<D-s>", "<ESC>:update<CR>", opts)
+keymap("n", "<D-s>", "<cmd>update<CR>", opts)
 keymap("v", "<D-s>", "<C-C>:update<CR><ESC>", opts)
 keymap("i", "<D-s>", "<C-O>:update<CR><ESC>", opts)
 -- Map Alt + s to save on linux in tmux (ffs)
-keymap("n", "<M-s>", "<ESC>:update<CR>", opts)
+keymap("n", "<M-s>", "<cmd>update<CR>", opts)
 keymap("v", "<M-s>", "<C-C>:update<CR><ESC>", opts)
 keymap("i", "<M-s>", "<C-O>:update<CR><ESC>", opts)
 -- center ctrl d and u
@@ -45,13 +45,13 @@ keymap("n", "<C-u>", "<C-u>zz", opts)
 -- paste over and keep in register
 keymap("x", "<leader>p", '"_dP', opts)
 -- Update vim config
-keymap("n", "<bslash>u", ":so ~/.config/nvim/init.lua<CR>", opts)
+keymap("n", "<bslash>u", "<cmd>so ~/.config/nvim/init.lua<CR>", opts)
 -- UTIL
 -- Vue
 keymap("n", "<leader>c", "/script<CR>ggn", opts)
 keymap("n", "<leader>v", "/template<CR>ggn", opts)
 -- love2d
-keymap("n", "<leader>r", ":AsyncRun -mode=term -rows=6 -listed=0 -scroll=1 -focus=0  love .<CR>", opts)
+keymap("n", "<leader>r", "<cmd>AsyncRun -mode=term -rows=6 -listed=0 -scroll=1 -focus=0  love .<CR>", opts)
 
 -- PLUGINS
 -- Telescope
@@ -69,28 +69,28 @@ keymap("n", "<bslash>sp", "<cmd>lua require'telescope'.extensions.project.projec
 keymap("n", "<bslash>sr", "<cmd>Telescope resume<CR>", opts)
 
 -- neo-tree
-keymap("n", "<C-t>", ":Neotree<CR>", opts)
-keymap("n", "<bslash>fs", ":Neotree reveal<CR>", opts)
+keymap("n", "<C-t>", "<cmd>Neotree<CR>", opts)
+keymap("n", "<bslash>fs", "<cmd>Neotree reveal<CR>", opts)
 -- bufferline
-keymap("n", "tg", ":BufferLinePick<CR>", opts)
+keymap("n", "tg", "<cmd>BufferLinePick<CR>", opts)
 -- keymap("n", "<bslash>w", ":BufferLineCloseLeft<CR>:BufferLineCloseRight<CR>", opts)
-keymap("n", "<bslash>w", ":w|%bd|e#|bd#<CR>", opts)
-keymap("n", "<bslash>q", ":Bdelete<CR>", opts)
+keymap("n", "<bslash>w", "<cmd>w|%bd|e#|bd#<CR>", opts)
+keymap("n", "<bslash>q", "<cmd>Bdelete<CR>", opts)
 -- keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", opts)
 -- keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", opts)
 -- whichkey
-keymap("n", "<bslash>a", ":WhichKey<CR>", opts)
+keymap("n", "<bslash>a", "<cmd>WhichKey<CR>", opts)
 -- toggleterm
 keymap("t", "<esc>", [[<C-\><C-n>]], opts)
 keymap("t", "jk", [[<C-\><C-n>]], opts)
 -- outline
 keymap("n", "<bslash>o", "<cmd>Outline<CR>", opts)
 -- tabby
-keymap("n", "<bslash>ta", ":$tabnew<CR>", opts)
-keymap("n", "<bslash>tc", ":tabclose<CR>", opts)
-keymap("n", "<bslash>to", ":tabonly<CR>", opts)
-keymap("n", "<bslash>tn", ":tabn<CR>", opts)
-keymap("n", "<bslash>tp", ":tabp<CR>", opts)
+keymap("n", "<bslash>ta", "<cmd>$tabnew<CR>", opts)
+keymap("n", "<bslash>tc", "<cmd>tabclose<CR>", opts)
+keymap("n", "<bslash>to", "<cmd>tabonly<CR>", opts)
+keymap("n", "<bslash>tn", "<cmd>tabn<CR>", opts)
+keymap("n", "<bslash>tp", "<cmd>tabp<CR>", opts)
 -- harpoon
 -- keymap("n", "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
 -- keymap("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<CR>", opts)
@@ -112,7 +112,7 @@ keymap("n", "<leader>gv", "<cmd>Octo issue list assignee=philvernon<CR>", opts)
 -- git
 keymap("n", "<leader>gd", "<cmd>Gitsigns diffthis<CR>", opts)
 keymap("n", "<leader>gD", "<cmd>Gitsigns diffthis develop<CR>", opts)
-keymap("n", "<leader>gs", ":Gitsigns show ", opts)
+keymap("n", "<leader>gs", "<cmd>Gitsigns show ", opts)
 keymap("n", "<leader>gS", "<cmd>Gitsigns show develop<CR>", opts)
 keymap("n", "<bslash>gg", "<cmd>Neogit kind=vsplit<CR>", opts)
 keymap("n", "<bslash>gs", "<cmd>Gitsigns stage_buffer<CR>", opts)
@@ -124,4 +124,7 @@ keymap("n", "<bslash>c", "<cmd>SessionRestore<CR>", opts)
 -- translate
 keymap("n", "<bslash>l", "<cmd>Translate<CR>", opts)
 -- checkboxes
-keymap("n", "<leader>x", "<cmd>:lua require('toggle-checkbox').toggle()<CR>", opts)
+keymap("n", "<leader>x", "<cmd>lua require('toggle-checkbox').toggle()<CR>", opts)
+keymap("n", "<C-e>", function()
+	vim.fn.jobstart({ "zsh", "-ic", "tmux-sesh" }, { detach = true })
+end, { silent = true, desc = "Open tmux-sesh" })
